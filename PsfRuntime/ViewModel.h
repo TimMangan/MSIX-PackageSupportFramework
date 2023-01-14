f7bb7d0260d6da6981b70bd80a26d908a26cf21f
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2023-01-14 09:39:29 -0500

    Dependencies for CreateProcessAsUser intercept

---
+++ PsfRuntime/ViewModel.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "ViewModel.g.h"
+
+namespace winrt::PsfRuntime::implementation
+{
+    struct ViewModel : ViewModelT<ViewModel>
+    {
+        ViewModel() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::PsfRuntime::factory_implementation
+{
+    struct ViewModel : ViewModelT<ViewModel, implementation::ViewModel>
+    {
+    };
+}
