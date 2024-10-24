ad2760d40cd0957e717a987ffd23a2ae0965ca2d
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2024-10-24 15:04:28 -0400

    Add Versioning to most PSF Components

--- Set-DllVersionByDate.ps1
+++ Set-DllVersionByDate.ps1
@@ -5,18 +5,22 @@
 
 
 Param( 
+	
 	[Parameter(Mandatory=$true)]
-	[string]$FilePathDll 
+	[string]$PathToStampVer,
+	[Parameter(Mandatory=$true)]
+	[string]$FilePathDll
 )
 
 	$fullDate = get-date
 	$curYear = $fullDate.Year
 	$curMonth = $fullDate.Month
-	$curDay = $fullDate.Date
-	$curMins = $fullDate.Hour * 60 + $fullDate.Minute
+	$curDay = $fullDate.Day
+	$curMins = (($fullDate.Hour) * 60) + $fullDate.Minute
 
-	Write-output "Set version ($curYear).$($curMonth).$($curDay).$($curMins) on file $($FilePathDll)"
-	ILMerge.exe "$($FilePathDll)" "/ver:$($curYear).$($curMonth).$($curDay).$($curMins)" "/out:$($FilePathDll)"
+	Write-output "Set version $($curYear).$($curMonth).$($curDay).$($CurMins) on file $($FilePathDll)"
+	#Write-output "cmd:Start-Process $($PathToStampVer) -ArgumentList" "-o4" "," "-f`"$($curYear).$($curMonth).$($curDay).$($curMins)`"" ","  "$($FilePathDll)"
+	Start-Process $PathToStampVer -ArgumentList "-o4", "-f`"$($curYear).$($curMonth).$($curDay).$($curMins)`"", "-p`"$($curYear).$($curMonth).$($curDay).$($curMins)`"", "$($FilePathDll)"
 	Write-output "Done."
-	write-host hi
+	
 
