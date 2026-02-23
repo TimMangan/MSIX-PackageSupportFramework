a9b404b63285ec8d44ed7ebf8cec7d96cf817272
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2026-02-22 21:18:33 -0500

    Logging

--- fixups/MFRFixup/dllmain.cpp
+++ fixups/MFRFixup/dllmain.cpp
@@ -43,105 +43,106 @@ extern "C" {
         if (LogLevel_DebugMaximum <= g_JsonDebugLevel)
         {
 #if DEBUG_NEW_FIXUPS_MFR
-            Log(LogLevel_DebugMaximum, "CopyFile Ansi=%p Wide=%p\n", &impl::CopyFileW.ansi, &impl::CopyFileW.wide);
-            Log(LogLevel_DebugMaximum, "CopyFile2 neutral=%p\n", &impl::CopyFile2);
-            Log(LogLevel_DebugMaximum, "CopyFileEx Ansi=%p Wide=%p\n", &impl::CopyFileExW.ansi, &impl::CopyFileExW.wide);
-            Log(LogLevel_DebugMaximum, "CreateDirectory  Ansi=%p Wide=%p\n", &impl::CreateDirectoryW.ansi, &impl::CreateDirectoryW.wide);
-            Log(LogLevel_DebugMaximum, "CreateDirectoryEx  Ansi=%p Wide=%p\n", &impl::CreateDirectoryExW.ansi, &impl::CreateDirectoryExW.wide);
-            Log(LogLevel_DebugMaximum, "CreateFile  Ansi=%p Wide=%p\n", &impl::CreateFileW.ansi, &impl::CreateFileW.wide);
-            Log(LogLevel_DebugMaximum, "CreateFile2  neutral=%p\n", &impl::CreateFile2);
-            Log(LogLevel_DebugMaximum, "CreateHardLink  Ansi=%p Wide=%p\n", &impl::CreateHardLinkW.ansi, &impl::CreateHardLinkW.wide);
-            Log(LogLevel_DebugMaximum, "CreateSymbolicLink  Ansi=%p Wide=%p\n", &impl::CreateSymbolicLinkW.ansi, &impl::CreateSymbolicLinkW.wide);
-            Log(LogLevel_DebugMaximum, "DeleteFile  Ansi=%p Wide=%p\n", &impl::DeleteFileW.ansi, &impl::DeleteFileW.wide);
-            Log(LogLevel_DebugMaximum, "FindClose  neutral=%p\n", &impl::FindClose);
-            Log(LogLevel_DebugMaximum, "FindFirstFile  Ansi=%p Wide=%p\n", &impl::FindFirstFileW.ansi, &impl::FindFirstFileW.wide);
-            Log(LogLevel_DebugMaximum, "FindFirstFileEx  Ansi=%p Wide=%p\n", &impl::FindFirstFileExW.ansi, &impl::FindFirstFileExW.wide);
-            Log(LogLevel_DebugMaximum, "FindNextFile  Ansi=%p Wide=%p\n", &impl::FindNextFileW.ansi, &impl::FindNextFileW.wide);
+            Log(LogLevel_DebugMaximum, "Mfr Intercepts:");
+            Log(LogLevel_DebugMaximum, "\tCopyFile Ansi=%p Wide=%p\n", &impl::CopyFileW.ansi, &impl::CopyFileW.wide);
+            Log(LogLevel_DebugMaximum, "\tCopyFile2 neutral=%p\n", &impl::CopyFile2);
+            Log(LogLevel_DebugMaximum, "\tCopyFileEx Ansi=%p Wide=%p\n", &impl::CopyFileExW.ansi, &impl::CopyFileExW.wide);
+            Log(LogLevel_DebugMaximum, "\tCreateDirectory  Ansi=%p Wide=%p\n", &impl::CreateDirectoryW.ansi, &impl::CreateDirectoryW.wide);
+            Log(LogLevel_DebugMaximum, "\tCreateDirectoryEx  Ansi=%p Wide=%p\n", &impl::CreateDirectoryExW.ansi, &impl::CreateDirectoryExW.wide);
+            Log(LogLevel_DebugMaximum, "\tCreateFile  Ansi=%p Wide=%p\n", &impl::CreateFileW.ansi, &impl::CreateFileW.wide);
+            Log(LogLevel_DebugMaximum, "\tCreateFile2  neutral=%p\n", &impl::CreateFile2);
+            Log(LogLevel_DebugMaximum, "\tCreateHardLink  Ansi=%p Wide=%p\n", &impl::CreateHardLinkW.ansi, &impl::CreateHardLinkW.wide);
+            Log(LogLevel_DebugMaximum, "\tCreateSymbolicLink  Ansi=%p Wide=%p\n", &impl::CreateSymbolicLinkW.ansi, &impl::CreateSymbolicLinkW.wide);
+            Log(LogLevel_DebugMaximum, "\tDeleteFile  Ansi=%p Wide=%p\n", &impl::DeleteFileW.ansi, &impl::DeleteFileW.wide);
+            Log(LogLevel_DebugMaximum, "\tFindClose  neutral=%p\n", &impl::FindClose);
+            Log(LogLevel_DebugMaximum, "\tFindFirstFile  Ansi=%p Wide=%p\n", &impl::FindFirstFileW.ansi, &impl::FindFirstFileW.wide);
+            Log(LogLevel_DebugMaximum, "\tFindFirstFileEx  Ansi=%p Wide=%p\n", &impl::FindFirstFileExW.ansi, &impl::FindFirstFileExW.wide);
+            Log(LogLevel_DebugMaximum, "\tFindNextFile  Ansi=%p Wide=%p\n", &impl::FindNextFileW.ansi, &impl::FindNextFileW.wide);
 
 #if FIXUP_FROM_KernelBase
-            Log(LogLevel_DebugMaximum, "(KernelBase)MoveFileExW  Wide=%p\n", kernelbaseimpl::MoveFileExWImpl);
+            Log(LogLevel_DebugMaximum, "\t(KernelBase)MoveFileExW  Wide=%p\n", kernelbaseimpl::MoveFileExWImpl);
 #endif
 
-            Log(LogLevel_DebugMaximum, "MoveFile  Ansi=%p Wide=%p\n", &impl::MoveFileW.ansi, &impl::MoveFileW.wide);
-            Log(LogLevel_DebugMaximum, "MoveFileEx  Ansi=%p Wide=%p\n", &impl::MoveFileExW.ansi, &impl::MoveFileExW.wide);
-            Log(LogLevel_DebugMaximum, "MoveFileWithProgress  Ansi=%p Wide=%p\n", &impl::MoveFileWithProgressW.ansi, &impl::MoveFileWithProgressW.wide);
-            Log(LogLevel_DebugMaximum, "RemoveDirectory  Ansi=%p Wide=%p\n", &impl::RemoveDirectoryW.ansi, &impl::RemoveDirectoryW.wide);
-            Log(LogLevel_DebugMaximum, "ReplaceFile  Ansi=%p Wide=%p\n", &impl::ReplaceFileW.ansi, &impl::ReplaceFileW.wide);
-            Log(LogLevel_DebugMaximum, "SetFileAttributes  Ansi=%p Wide=%p\n", &impl::SetFileAttributesW.ansi, &impl::SetFileAttributesW.wide);
+            Log(LogLevel_DebugMaximum, "\tMoveFile  Ansi=%p Wide=%p\n", &impl::MoveFileW.ansi, &impl::MoveFileW.wide);
+            Log(LogLevel_DebugMaximum, "\tMoveFileEx  Ansi=%p Wide=%p\n", &impl::MoveFileExW.ansi, &impl::MoveFileExW.wide);
+            Log(LogLevel_DebugMaximum, "\tMoveFileWithProgress  Ansi=%p Wide=%p\n", &impl::MoveFileWithProgressW.ansi, &impl::MoveFileWithProgressW.wide);
+            Log(LogLevel_DebugMaximum, "\tRemoveDirectory  Ansi=%p Wide=%p\n", &impl::RemoveDirectoryW.ansi, &impl::RemoveDirectoryW.wide);
+            Log(LogLevel_DebugMaximum, "\tReplaceFile  Ansi=%p Wide=%p\n", &impl::ReplaceFileW.ansi, &impl::ReplaceFileW.wide);
+            Log(LogLevel_DebugMaximum, "\tSetFileAttributes  Ansi=%p Wide=%p\n", &impl::SetFileAttributesW.ansi, &impl::SetFileAttributesW.wide);
 
-            Log(LogLevel_DebugMaximum, "GetFileAttributes  Ansi=%p Wide=%p\n", &impl::GetFileAttributesW.ansi, &impl::GetFileAttributesW.wide);
-            Log(LogLevel_DebugMaximum, "GetFileAttributesEx  Ansi=%p Wide=%p\n", &impl::GetFileAttributesExW.ansi, &impl::GetFileAttributesExW.wide);
-            Log(LogLevel_DebugMaximum, "SetFileAttributes  Ansi=%p Wide=%p\n", &impl::SetFileAttributesW.ansi, &impl::SetFileAttributesW.wide);
-            Log(LogLevel_DebugMaximum, "GetPrivateProfileInt  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileIntW.ansi, &impl::GetPrivateProfileIntW.wide);
-            Log(LogLevel_DebugMaximum, "GetPrivateProfileSection  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileSectionW.ansi, &impl::GetPrivateProfileSectionW.wide);
-            Log(LogLevel_DebugMaximum, "GetPrivateProfileSectionNames  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileSectionNamesW.ansi, &impl::GetPrivateProfileSectionNamesW.wide);
-            Log(LogLevel_DebugMaximum, "GetPrivateProfileString  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileStringW.ansi, &impl::GetPrivateProfileStringW.wide);
-            Log(LogLevel_DebugMaximum, "GetPrivateProfileStruct  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileStructW.ansi, &impl::GetPrivateProfileStructW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetFileAttributes  Ansi=%p Wide=%p\n", &impl::GetFileAttributesW.ansi, &impl::GetFileAttributesW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetFileAttributesEx  Ansi=%p Wide=%p\n", &impl::GetFileAttributesExW.ansi, &impl::GetFileAttributesExW.wide);
+            Log(LogLevel_DebugMaximum, "\tSetFileAttributes  Ansi=%p Wide=%p\n", &impl::SetFileAttributesW.ansi, &impl::SetFileAttributesW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetPrivateProfileInt  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileIntW.ansi, &impl::GetPrivateProfileIntW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetPrivateProfileSection  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileSectionW.ansi, &impl::GetPrivateProfileSectionW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetPrivateProfileSectionNames  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileSectionNamesW.ansi, &impl::GetPrivateProfileSectionNamesW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetPrivateProfileString  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileStringW.ansi, &impl::GetPrivateProfileStringW.wide);
+            Log(LogLevel_DebugMaximum, "\tGetPrivateProfileStruct  Ansi=%p Wide=%p\n", &impl::GetPrivateProfileStructW.ansi, &impl::GetPrivateProfileStructW.wide);
 
-            //Log(LogLevel_DebugMaximum, "GetCurrentDirectory  Ansi=%p Wide=%p\n", &::GetCurrentDirectoryA, &::GetCurrentDirectoryW);
-            Log(LogLevel_DebugMaximum, "SetCurrentDirectory  Ansi=%p Wide=%p\n", &impl::SetCurrentDirectoryW.wide, &impl::SetCurrentDirectoryW.wide);
+            //Log(LogLevel_DebugMaximum, "\tGetCurrentDirectory  Ansi=%p Wide=%p\n", &::GetCurrentDirectoryA, &::GetCurrentDirectoryW);
+            Log(LogLevel_DebugMaximum, "\tSetCurrentDirectory  Ansi=%p Wide=%p\n", &impl::SetCurrentDirectoryW.wide, &impl::SetCurrentDirectoryW.wide);
 
-            Log(LogLevel_DebugMaximum, "WritePrivateProfileSection  Ansi=%p Wide=%p\n", &impl::WritePrivateProfileSectionW.wide, &impl::WritePrivateProfileSectionW.wide);
-            Log(LogLevel_DebugMaximum, "WritePrivateProfileString  Ansi=%p Wide=%p\n", &impl::WritePrivateProfileStringW.ansi, &impl::WritePrivateProfileStringW.wide);
-            Log(LogLevel_DebugMaximum, "WritePrivateProfileStruct  Ansi=%p Wide=%p\n", &impl::WritePrivateProfileStructW.ansi, &impl::WritePrivateProfileStructW.wide);
+            Log(LogLevel_DebugMaximum, "\tWritePrivateProfileSection  Ansi=%p Wide=%p\n", &impl::WritePrivateProfileSectionW.wide, &impl::WritePrivateProfileSectionW.wide);
+            Log(LogLevel_DebugMaximum, "\tWritePrivateProfileString  Ansi=%p Wide=%p\n", &impl::WritePrivateProfileStringW.ansi, &impl::WritePrivateProfileStringW.wide);
+            Log(LogLevel_DebugMaximum, "\tWritePrivateProfileStruct  Ansi=%p Wide=%p\n", &impl::WritePrivateProfileStructW.ansi, &impl::WritePrivateProfileStructW.wide);
 
-            //Log(LogLevel_DebugMaximum, "SearchPath  Ansi=%p Wide=%p\n", &::SearchPathA, &::SearchPathW);
-
+            //Log(LogLevel_DebugMaximum, "\tSearchPath  Ansi=%p Wide=%p\n", &::SearchPathA, &::SearchPathW);
+             
 #if FIXUP_ORIGINAL_SHELLEXECUTE
-            Log(LogLevel_DebugMaximum, "ShellExecute  Ansi=%p Wide=%p\n", &::ShellExecuteA, &::ShellExecuteW);
+            Log(LogLevel_DebugMaximum, "\tShellExecute  Ansi=%p Wide=%p\n", &::ShellExecuteA, &::ShellExecuteW);
 #endif
 #if FIXUP_ORIGINAL_SHELLEXECUTEEX
-            Log(LogLevel_DebugMaximum, "SHellExecuteEx  Ansi=%p Wide=%p\n", &::ShellExecuteExA, &::ShellExecuteExW);
+            Log(LogLevel_DebugMaximum, "\tSHellExecuteEx  Ansi=%p Wide=%p\n", &::ShellExecuteExA, &::ShellExecuteExW);
 #endif
 
 #if Intercept_WindowsStorage
 #if DO_WS_Shex_A
             if (windowsstorageimpl::ShellExecuteAImpl == nullptr)
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteA to  Ansi=NULL\n");
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteA to  Ansi=NULL\n");
             else
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteA to  Ansi=%p\n", windowsstorageimpl::ShellExecuteAImpl);
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteA to  Ansi=%p\n", windowsstorageimpl::ShellExecuteAImpl);
 #endif
 #if DO_WS_Shex_W
             if (windowsstorageimpl::ShellExecuteWImpl == nullptr)
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteW to  Wide=NULL\n");
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteW to  Wide=NULL\n");
             else
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteW to  Wide=%p\n", windowsstorageimpl::ShellExecuteWImpl);
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteW to  Wide=%p\n", windowsstorageimpl::ShellExecuteWImpl);
 #endif
 #if DO_WS_ShexEx_A
             if (windowsstorageimpl::ShellExecuteExAImpl == nullptr)
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteExA to  Ansi=NULL\n");
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteExA to  Ansi=NULL\n");
             else
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteExA  Ansi=%p\n", &windowsstorageimpl::ShellExecuteExAImpl);
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteExA  Ansi=%p\n", &windowsstorageimpl::ShellExecuteExAImpl);
 #endif
 #if DO_WS_ShexEx_W
             if (windowsstorageimpl::ShellExecuteExWImpl == nullptr)
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteExW to  Wide=NULL\n");
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteExW to  Wide=NULL\n");
             else
-                Log(LogLevel_DebugMaximum, "(windows.storage)ShellExecuteExW  Wide=%p\n", &windowsstorageimpl::ShellExecuteExWImpl);
-            Log(LogLevel_DebugMaximum, "WindowsStorage Fixups loaded.\n");
+                Log(LogLevel_DebugMaximum, "\t(windows.storage)ShellExecuteExW  Wide=%p\n", &windowsstorageimpl::ShellExecuteExWImpl);
+            Log(LogLevel_DebugMaximum, "\tWindowsStorage Fixups loaded.\n");
 #endif
 #endif
 
 #ifdef Intercept_NTDLL
 #ifdef DO_Intercept_NtCreateFile
             if (ntdllimpl::NtCreateFileImpl != nullptr)
-                Log(LogLevel_DebugMaximum, "(ntdll)NtCreateFile Neutral=%p\n", &ntdllimpl::NtCreateFileImpl);
+                Log(LogLevel_DebugMaximum, "\t(ntdll)NtCreateFile Neutral=%p\n", &ntdllimpl::NtCreateFileImpl);
 #endif
 #ifdef DO_Intercept_NtOpenFile
             if (ntdllimpl::NtOpenFileImpl != nullptr)
-                Log(LogLevel_DebugMaximum, "(ntdll)NtOpenFile Neutral=%p\n", &ntdllimpl::NtOpenFileImpl);
+                Log(LogLevel_DebugMaximum, "\t(ntdll)NtOpenFile Neutral=%p\n", &ntdllimpl::NtOpenFileImpl);
 #endif
 #ifdef DO_Intercept_NtQueryDirectoryFile
             if (ntdllimpl::NtQueryDirectoryFileImpl != nullptr)
-                Log(LogLevel_DebugMaximum, "(ntdll)NtQueryDirectoryFile Neutral=%p\n", &ntdllimpl::NtQueryDirectoryFileImpl);
+                Log(LogLevel_DebugMaximum, "\t(ntdll)NtQueryDirectoryFile Neutral=%p\n", &ntdllimpl::NtQueryDirectoryFileImpl);
 #endif
 
 #ifdef DO_Intercept_NtQueryDirectoryFileEx
             if (ntdllimpl::NtQueryDirectoryFileExImpl != nullptr)
-                Log(LogLevel_DebugMaximum, "(ntdll)NtQueryDirectoryFileEx Neutral=%p\n", &ntdllimpl::NtQueryDirectoryFileExImpl);
+                Log(LogLevel_DebugMaximum, "\t(ntdll)NtQueryDirectoryFileEx Neutral=%p\n", &ntdllimpl::NtQueryDirectoryFileExImpl);
 #endif
 
-            Log(LogLevel_DebugMaximum, "ntdll Fixups loaded.\n");
+            Log(LogLevel_DebugMaximum, "\tntdll Fixups loaded.\n");
 #endif
 
 #endif
